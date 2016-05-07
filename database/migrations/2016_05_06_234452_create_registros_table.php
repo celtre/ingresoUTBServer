97acd8b762c636e3128fa361111cfd1740b27277
vagrant <vagrant@homestead>
2016-05-07 00:10:59 +0000

    Queda faltanto implementacion de controlador y la ruta

---
+++ database/migrations/2016_05_06_234452_create_registros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            //campos de la tabla
+            $table->string('codigo');
+            $table->string('autor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('registros');
+    }
+}
